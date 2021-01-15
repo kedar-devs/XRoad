d53738fda770b4dbcf9205e5d8c9a1e5b164a02f
Deborah Mendes <deb.mendes.9@gmail.com>
2021-01-15 14:21:49 +0530

    Getting Current User location

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import GetUserLocation from "./components/GetUserLocation";
 
 function App() {
+  const [currentLocation, setCurrentLocation] = useState({
+    lat: 15.292158,
+    lng: 73.969542,
+  });
+
+  useEffect(() => {
+    console.log(currentLocation);
+  }, [currentLocation]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {currentLocation.lat}
+      <GetUserLocation setCurrentLocation={setCurrentLocation} />
     </div>
   );
 }
