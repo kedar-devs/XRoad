cee25b79fec50d99ae31cfa444221451bf467076
Riddhi Siddarkar <riddhisiddarkar@gmail.com>
2021-01-16 00:12:52 +0530

    adding then dashboard component

---
+++ client/src/components/ComplaintsDisplay/ComplaintsDisplay.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./ComplaintsDisplay.css";
+const ComplaintsDisplay = () => {
+  return <div className="ComplaintsDisplay"></div>;
+};
+
+export default ComplaintsDisplay;
