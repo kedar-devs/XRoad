664188205249ce9f2858927ac6ac588cc0ce7ed4
Riddhi Siddarkar <riddhisiddarkar@gmail.com>
2021-01-15 22:48:47 +0530

    connecting to the backend

--- client/src/components/Forms/CitizenForm/CitizenForm.jsx
+++ client/src/components/Forms/CitizenForm/CitizenForm.jsx
@@ -1,220 +1,240 @@
-import React, { Component } from 'react'
-import Button from '../../UI/Button/Button'
-import classes from './CitizenForm.module.css'
-import Input from '../../UI/Input/Input'
-import Spinner from '../../UI/Spinner/Spinner'
-
+import React, { Component } from "react";
+import Button from "../../UI/Button/Button";
+import classes from "./CitizenForm.module.css";
+import Input from "../../UI/Input/Input";
+import Spinner from "../../UI/Spinner/Spinner";
+import axios from "axios";
 class CitizenForm extends Component {
-	state = {
-		orderForm: {
-			name: {
-				elementType: 'input',
-				elementConfig: {
-					type: 'text',
-					placeholder: 'Name',
-				},
-				value: '',
-				validation: {
-					required: true,
-				},
-				valid: false,
-				touched: false,
-			},
+  state = {
+    orderForm: {
+      name: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Name",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+      },
 
-			email: {
-				elementType: 'input',
-				elementConfig: {
-					type: 'email',
-					placeholder: 'Email',
-				},
-				value: '',
-				validation: {
-					required: true,
-				},
-				valid: false,
-				touched: false,
-			},
-			latitude: {
-				elementType: 'input',
-				elementConfig: {
-					type: 'number',
-					placeholder: 'Latitude',
-				},
-				value: '',
-				validation: {
-					required: true,
-				},
-				valid: !false,
-				touched: false,
-			},
-			longitude: {
-				elementType: 'input',
-				elementConfig: {
-					type: 'number',
-					placeholder: 'Longitude',
-				},
-				value: '',
-				validation: {
-					required: true,
-				},
-				valid: !false,
-				touched: false,
-			},
-			priority: {
-				elementType: 'select',
-				elementConfig: {
-					options: [
-						{ value: 1, displayValue: 'Priority 1 (Low)' },
-						{ value: 2, displayValue: 'Priority 2 (Medium)' },
-						{ value: 3, displayValue: 'Priority 3 (High)' },
-					],
-				},
-				value: '2',
-				validation: {},
-				valid: true,
-			},
-			description: {
-				elementType: 'textarea',
-				elementConfig: {
-					placeholder: 'Description',
-				},
-				value: '',
-				validation: {},
-				valid: true,
-			},
-			image: {
-				elementType: 'input',
-				elementConfig: {
-					placeholder: 'Image',
-					type: 'file',
-				},
-				value: '',
-				validation: {
-					required: true,
-				},
-				valid: true,
-			},
-		},
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Email",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      latitude: {
+        elementType: "input",
+        elementConfig: {
+          type: "number",
+          placeholder: "Latitude",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: !false,
+        touched: false,
+      },
+      longitude: {
+        elementType: "input",
+        elementConfig: {
+          type: "number",
+          placeholder: "Longitude",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: !false,
+        touched: false,
+      },
+      priority: {
+        elementType: "select",
+        elementConfig: {
+          options: [
+            { value: 1, displayValue: "Priority 1 (Low)" },
+            { value: 2, displayValue: "Priority 2 (Medium)" },
+            { value: 3, displayValue: "Priority 3 (High)" },
+          ],
+        },
+        value: "2",
+        validation: {},
+        valid: true,
+      },
+      description: {
+        elementType: "textarea",
+        elementConfig: {
+          placeholder: "Description",
+        },
+        value: "",
+        validation: {},
+        valid: true,
+      },
+      image: {
+        elementType: "input",
+        elementConfig: {
+          placeholder: "Image",
+          type: "file",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: true,
+      },
+    },
 
-		loading: false,
-		formIsValid: false,
-		image: null,
-	}
+    loading: false,
+    formIsValid: false,
+    image: null,
+  };
 
-	componentDidMount() {
-		if ('geolocation' in navigator) {
-			// console.log('Available')
-			const that = this
-			navigator.geolocation.getCurrentPosition(function (position) {
-				console.log('Latitude is :', position.coords.latitude)
-				console.log('Longitude is :', position.coords.longitude)
+  componentDidMount() {
+    if ("geolocation" in navigator) {
+      // console.log('Available')
+      const that = this;
+      navigator.geolocation.getCurrentPosition(function (position) {
+        console.log("Latitude is :", position.coords.latitude);
+        console.log("Longitude is :", position.coords.longitude);
 
-				const updatedForm = { ...that.state.orderForm }
-				const lat = { ...updatedForm['latitude'] }
-				const lon = { ...updatedForm['longitude'] }
+        const updatedForm = { ...that.state.orderForm };
+        const lat = { ...updatedForm["latitude"] };
+        const lon = { ...updatedForm["longitude"] };
 
-				lat.value = position.coords.latitude
-				lon.value = position.coords.longitude
-				updatedForm['latitude'] = lat
-				updatedForm['longitude'] = lon
+        lat.value = position.coords.latitude;
+        lon.value = position.coords.longitude;
+        updatedForm["latitude"] = lat;
+        updatedForm["longitude"] = lon;
 
-				that.setState({ orderForm: updatedForm })
-			})
-		} else {
-			// console.log('Not Available')
-		}
-	}
+        that.setState({ orderForm: updatedForm });
+      });
+    } else {
+      // console.log('Not Available')
+    }
+  }
 
-	checkValidity = (value, rules) => {
-		let isValid = true
+  checkValidity = (value, rules) => {
+    let isValid = true;
 
-		if (rules.required) {
-			isValid = value.trim() !== '' && isValid
-		}
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
 
-		return isValid
-	}
+    return isValid;
+  };
 
-	inputChangedHandler = (event, inputIdentifier) => {
-		// console.log(event.target.value)
-		const updatedOrderForm = { ...this.state.orderForm }
-		const updatedFormElement = { ...updatedOrderForm[inputIdentifier] }
+  inputChangedHandler = (event, inputIdentifier) => {
+    // console.log(event.target.value)
+    const updatedOrderForm = { ...this.state.orderForm };
+    const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
 
-		updatedFormElement.value = event.target.value
+    updatedFormElement.value = event.target.value;
 
-		if (updatedFormElement.elementConfig.type === 'file') {
-			this.setState({ image: event.target.files[0] })
-		}
+    if (updatedFormElement.elementConfig.type === "file") {
+      this.setState({ image: event.target.files[0] });
+    }
 
-		updatedFormElement.valid = this.checkValidity(
-			updatedFormElement.value,
-			updatedFormElement.validation
-		)
-		updatedFormElement.touched = true
-		updatedOrderForm[inputIdentifier] = updatedFormElement
+    updatedFormElement.valid = this.checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validation
+    );
+    updatedFormElement.touched = true;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
 
-		let formIsValid = true
-		for (let inputIdentifier in updatedOrderForm) {
-			formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid
-		}
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
 
-		this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid })
-		// console.log(updatedFormElement)
-	}
+    this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
+    // console.log(updatedFormElement)
+  };
 
-	submitHandler = (event) => {
-		event.preventDefault()
-		const formData = {}
-		for (let formElementIdentifier in this.state.orderForm) {
-			formData[formElementIdentifier] = this.state.orderForm[
-				formElementIdentifier
-			].value
-		}
-		// Image Upload
-		const image = this.state.image
-		console.log(image)
-		alert(JSON.stringify(formData, null, 2))
-	}
+  submitHandler = (event) => {
+    event.preventDefault();
+    const formData = {};
+    // for (let formElementIdentifier in this.state.orderForm) {
+    // 	formData[formElementIdentifier] = this.state.orderForm[
+    // 		formElementIdentifier
+    // 	].value
+    // }
+    // Image Upload
+    const image = this.state.image;
+    console.log(image);
+    const data = new FormData();
+    data.set("encType", "multipart/form-data");
+    data.append("name", this.state.orderForm.name.value);
+    data.append("email", this.state.orderForm.email.value);
+    data.append("latitude", this.state.orderForm.latitude.value);
+    data.append("longitude", this.state.orderForm.longitude.value);
+    data.append("priority", this.state.orderForm.priority.value);
+    data.append("description", this.state.orderForm.description.value);
+    data.append("file", this.state.image);
+    const multerimage = URL.createObjectURL(this.state.image);
+    // alert(multerimage);
+    axios
+      .post("http://localhost:5000/complain/addcomplain", data)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-	render() {
-		const formElementsArray = []
-		for (let key in this.state.orderForm) {
-			formElementsArray.push({
-				id: key,
-				config: this.state.orderForm[key],
-			})
-		}
-		let form = (
-			<form onSubmit={this.submitHandler}>
-				{formElementsArray.map((formElement) => (
-					<Input
-						key={formElement.id}
-						elementType={formElement.config.elementType}
-						elementConfig={formElement.config.elementConfig}
-						value={formElement.config.value}
-						invalid={!formElement.config.valid}
-						shouldValidata={formElement.config.validation}
-						touched={formElement.config.touched}
-						changed={(event) => {
-							this.inputChangedHandler(event, formElement.id)
-						}}
-					/>
-				))}
-				<Button disabled={!this.state.formIsValid}>Add</Button>
-			</form>
-		)
+    // alert(JSON.stringify(formData, null, 2));
+  };
 
-		if (this.state.loading) {
-			form = <Spinner />
-		}
+  render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key],
+      });
+    }
+    let form = (
+      <form onSubmit={this.submitHandler} encType="multipart/formdata">
+        {formElementsArray.map((formElement) => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidata={formElement.config.validation}
+            touched={formElement.config.touched}
+            changed={(event) => {
+              this.inputChangedHandler(event, formElement.id);
+            }}
+          />
+        ))}
+        <Button disabled={!this.state.formIsValid}>Add</Button>
+      </form>
+    );
 
-		return (
-			<div className={classes.CitizenForm}>
-				<h1>Add Complaint</h1>
-				{form}
-			</div>
-		)
-	}
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
+
+    return (
+      <div className={classes.CitizenForm}>
+        <h1>Add Complaint</h1>
+        {form}
+      </div>
+    );
+  }
 }
 
-export default CitizenForm
+export default CitizenForm;
--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 const express = require("express");
-const cors=require('cors')
-const path=require('path')
-const fileUpload = require('express-fileupload')
+const cors = require("cors");
+const path = require("path");
+const fileUpload = require("express-fileupload");
 // const StudentRoute = require("./db/routes/Student.route")
 // const ClassworkRoute = require("./db/routes/Classwork.route")
 // const RecruitorRoute = require("./db/routes/Recruitor.route")
 // const Bankhata=require("./db/routes/Bank.route")
-const ComplainRoute= require("./Routes/complain.route")
+const ComplainRoute = require("./Routes/complain.route");
 require("./mongoose");
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(fileUpload())
-app.use('/Data',express.static(path.join(__dirname+'/Model/Data')))
-app.use('/complain',ComplainRoute)
+app.use(fileUpload());
+app.use("/Data", express.static(path.join(__dirname + "/Model/Data")));
+app.use("/complain", ComplainRoute);
 // app.use('/Authority',RecruitorRoute)
 // app.use('/User',ClassworkRoute)
 // app.use('/something',Bankhata)
