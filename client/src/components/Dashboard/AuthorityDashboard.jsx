ca35af1e66f77eed77fcd9b1e22230eace68abfa
Shivam Naik <shivamnaik8239@gmail.com>
2021-01-16 13:03:12 +0530

    Change styling of doughnut chart

--- client/src/components/Dashboard/AuthorityDashboard.jsx
+++ client/src/components/Dashboard/AuthorityDashboard.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
 import classes from './AuthorityDashboard.module.css'
 import DoughnutChart from './DoughnutChart'
 
 const AuthorityDashboard = (props) => {
 	return (
-		<div container className={classes.Root}>
-			<DoughnutChart />
-		</div>
+		<Grid container className={classes.Root} direction='row'>
+			<Grid item container xs={12} md={6}>
+				<DoughnutChart />
+			</Grid>
+			<Grid item container xs={12} md={6}>
+				Complaints
+			</Grid>
+		</Grid>
 	)
 }
 
