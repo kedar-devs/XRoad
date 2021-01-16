709426f1630a2ddfeae2fb7b5f58394fad8428a7
Shivam Naik <shivamnaik8239@gmail.com>
2021-01-16 13:34:45 +0530

    Restructure and style some component

--- client/src/components/Dashboard/AuthorityDashboard.jsx
+++ client/src/components/Dashboard/AuthorityDashboard.jsx
@@ -2,12 +2,27 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 import classes from './AuthorityDashboard.module.css'
 import DoughnutChart from './DoughnutChart'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const AuthorityDashboard = (props) => {
+	const [status, changeStatus] = useState({
+		x: ['Pending', 'Level-1', 'Level-2', 'Completed'],
+		y: null,
+	})
+
+	useEffect(() => {
+		console.log('tst');
+		changeStatus({
+			x: ['Pending', 'Level-1', 'Level-2', 'Completed'],
+			y: [12, 5, 3, 8],
+		})
+	}, [])
+
 	return (
 		<Grid container className={classes.Root} direction='row'>
 			<Grid item container xs={12} md={6}>
-				<DoughnutChart />
+				<DoughnutChart status={status} />
 			</Grid>
 			<Grid item container xs={12} md={6}>
 				Complaints
